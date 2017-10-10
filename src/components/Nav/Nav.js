dbbb58142f30512f68025ff162f197d91c3056c5
zhagm <zhagdev@gmail.com>
2017-10-11 05:04:08 +0700

    nav styled, need to change font but otherwise ready

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { NavLink, Link } from 'react-router-dom';
-import { container, navContainer, link } from './styles.css';
+import { container, navContainer, link, active } from './styles.css';
 
 export default function Nav (props) {
   return (
     <div className={container}>
       <nav className={navContainer}>
         <ul>
-          <li><NavLink activeClassName='active' to='/template' className={link}>{'WELCOME'}</NavLink></li>
-          <li><NavLink activeClassName='active' to='/template' className={link}>{'MENU'}</NavLink></li>
-          <li><NavLink activeClassName='active' to='/template' className={link}>{'LOCATIONS'}</NavLink></li>
-          <li><NavLink activeClassName='active' to='/template' className={link}>{'ORDER'}</NavLink></li>
-          <li><NavLink activeClassName='active' to='/template' className={link}>{'CONTACT US'}</NavLink></li>
+          <li><NavLink activeClassName={active} exact to='/' className={link}>{'WELCOME'}</NavLink></li>
+          <li><NavLink activeClassName={active} to='/menu' className={link}>{'MENU'}</NavLink></li>
+          <li><NavLink activeClassName={active} to='/locations' className={link}>{'LOCATIONS'}</NavLink></li>
+          <li><NavLink activeClassName={active} to='/order' className={link}>{'ORDER'}</NavLink></li>
+          <li><NavLink activeClassName={active} to='/contact' className={link}>{'CONTACT US'}</NavLink></li>
         </ul>
       </nav>
     </div>
