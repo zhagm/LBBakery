f0a575617f7edd3c9837fa6cfc632fda7eaba15d
zhagm <zhagdev@gmail.com>
2017-10-11 02:25:38 +0700

    init, template

---
+++ src/containers/App/AppContainer.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Route, BrowserRouter, Redirect, Switch } from 'react-router-dom';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import { Nav } from 'components';
+import { HomeContainer, TemplateContainer } from 'containers';
+import * as thingActionCreators from 'redux/modules/thing';
+
+class App extends Component {
+  render () {
+    return (
+      <BrowserRouter>
+        <div>
+          <Nav />
+          <Switch>
+            <Route exact={true} path='/' component={HomeContainer} />
+            <Route path='/template' component={TemplateContainer} />
+            <Route render={() => <h1>{'404'}</h1>} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+
+App.propTypes = {};
+
+export default connect((state) => ({
+  state: state
+}), (dispatch) => (
+  bindActionCreators(thingActionCreators, dispatch)
+))(App);
