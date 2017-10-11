a8e087d46cdd66980b82056da8c302b4a3f5de7e
zhagm <zhagdev@gmail.com>
2017-10-12 03:06:09 +0700

    submenu dymanically made based on state

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -2,11 +2,72 @@ import React, { Component } from 'react';
 import { SubMenu } from 'components';
 
 export default class Menu extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      active: 'Pastries'
+    }
+  }
   render () {
     return (
       <div>
-        <SubMenu />
+        
+        { props[this.state.active].map((sub, i, a) => (
+          <SubMenu category={sub.category} items={sub.items} />
+        ))}
       </div>
     );
   }
+}
+
+
+let props =
+{
+  'Pastries': [
+    {
+      category: 'Croissants',
+      items: [
+        { item: 'Butter Croissant', price: 1.95 },
+        { item: 'Chocolate Croissant', price: 2.05 },
+        { item: 'Almond Croissant', price: 2.95 },
+        { item: 'Almond Chocolate Croissant', price: 3.05 }
+      ]
+    },
+    {
+      category: 'Other Pastries',
+      items: [
+        { item: 'Canelé', price: 1.10 },
+        { item: 'Chocolate Moelleux', price: 2.75 },
+        { item: 'Bread Pudding', price: 2.05 },
+        { item: 'Strudel', price: 7.95 },
+        { item: 'Raisin Whirl', price: 2.45 },
+        { item: 'Coconut Rock', price: 1.30 },
+        { item: 'Eclair', price: 1.95 },
+        { item: 'Kouign-amann', price: 3.50 },
+        { item: 'Apple Turnover', price: 2.55 }
+      ]
+    },
+    {
+      category: 'Mini Pastries',
+      items: [
+        { item: 'Mini Croissant', price: 1.20 },
+        { item: 'Mini Chocolate Croissant', price: 1.20 },
+        { item: 'Mini Raisin Whirl', price: 1.20 },
+        { item: 'Mini Canelé', price: 0.55 },
+        { item: 'Mini Apple Turnover', price: 1.50 }
+      ]
+    }
+  ],
+  'head': [
+    {
+      category: 'subhead',
+      items: [
+        { item: 'Item Name', price: 1.00 },
+        { item: 'Item Name', price: 1.00 },
+        { item: 'Item Name', price: 1.00 },
+        { item: 'Item Name', price: 1.00 }
+      ]
+    },
+  ]
 }
